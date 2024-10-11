8e83663b1e0d544b630f1611b0842454a6a5fae3
Arham Hussain 🥈 <153752282+ARHAM5432@users.noreply.github.com>
2024-10-11 12:00:04 +0500

    ✔

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-Assalamualikum!
+var tareekh = new Date();
+console.log(tareekh.getDay());
+console.log(tareekh.getMonth());
+console.log(tareekh.getDate());
+console.log(tareekh.getFullYear());
+console.log(tareekh.getHours());
+console.log(tareekh.getMinutes());
+console.log(tareekh.getSeconds());
+console.log(tareekh.getMilliseconds());
+console.log(tareekh.getTime());
+
+// Question 1 🎶
+console.log(new Date("Dec 5 2015"));
+
+// Question 4 🎶
+var day = new Date().getDay()
+// day == 6 || day == 0 ? alert("It's Fun day!") : false
+
+// Question 5 🎶
+var date_2 = new Date().getDate();
+// date_2 < 15 ? alert("First 15 days of month") : alert("Last days of month")
+
+
+// Question 6 🎶
+var now = new Date().getHours();
+// now < 12 ? alert("Its A.M") : alert("It's P.M")
+
+// Question 7 🎶
+var laterDate = new Date(2020, 11, 31);
+console.log(laterDate);
+
+
+// Question 8 🎶
+var ramzanStart = new Date(2015, 5, 18);
+var today = new Date();
+var timeDiff = today - ramzanStart;
+var daysPassed = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+alert(daysPassed + " days have passed since 1st Ramadan.");
