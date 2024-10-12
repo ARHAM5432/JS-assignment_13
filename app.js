d55dff8a5ada55268ca1a7eeb1d5587551e6c73a
Arham Hussain 🥈 <153752282+ARHAM5432@users.noreply.github.com>
2024-10-12 17:43:30 +0500

    ✔

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ console.log(tareekh.getTime());
 // Question 1 🎶
 console.log(new Date("Dec 5 2015"));
 
+//  Question 2 🎶
+ var months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+ console.log(months[rightNow.getMonth()]);
+
+// Question 3 🎶 
+ var days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+ console.log(days[rightNow.getDay()]);
+
 // Question 4 🎶
 var day = new Date().getDay()
 // day == 6 || day == 0 ? alert("It's Fun day!") : false
@@ -20,7 +28,6 @@ var day = new Date().getDay()
 var date_2 = new Date().getDate();
 // date_2 < 15 ? alert("First 15 days of month") : alert("Last days of month")
 
-
 // Question 6 🎶
 var now = new Date().getHours();
 // now < 12 ? alert("Its A.M") : alert("It's P.M")
@@ -28,7 +35,6 @@ var now = new Date().getHours();
 // Question 7 🎶
 var laterDate = new Date(2020, 11, 31);
 console.log(laterDate);
-
 
 // Question 8 🎶
 var ramzanStart = new Date(2015, 5, 18);
